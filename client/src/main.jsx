a4e9aa1a9f424084cf1f5e8b5057413f107bb852
Aan Bahudin <aan.bahudin11@gmail.com>
2024-12-27 10:47:15 +0800

    adding toastify for notification

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import './index.css'
 import App from './App.jsx'
 import { render } from 'react-dom'
+import { ToastContainer } from 'react-toastify'
 
 
 render(
   <StrictMode>
     <App />
+    <ToastContainer position='top-center' />
   </StrictMode>, document.getElementById('root')
 )
