588ab8f3e43a1d8fb668a5866992cbf9f61b22a8
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-14 11:55:10 +0800

    refactor register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -102,7 +102,7 @@ const Register = () => {
           </button>
 
           <div className='w-[80%] flex items-start justify-start gap-x-2 mt-4'>
-            <input type="checkbox" className='outline-none' name="aggrement" id="aggrement" required />
+            <input type="checkbox" className='outline-none'required />
             <label htmlFor="aggrement" className='text-[12px] text-slate-700/80'>Dengan menyetujui proses daftar ini, Anda menerima dan setuju untuk mematuhi <span className='text-blue/80'> Ketentuan Layanan, Kebijakan Privasi, </span> serta semua aturan dan regulasi yang berlaku</label>
           </div>
 
@@ -121,65 +121,3 @@ const Register = () => {
 }
 
 export default Register
-
-
-// <div className='w-fullScreen h-fullScreen overflow-hidden grid grid-cols-2'>
-//   <Form method='POST' className="w-full h-full flex items-center justify-items-start col-span-1 bg-[url('/src/assets/images/gradient.jpg')] bg-no-repeat bg-cover">
-//         <div className="w-[80%] mx-auto ">
-//           <h1 className='font-semibold text-2xl'>Bergabung bersama kami</h1>
-//           <p className='text-sm mt-2 mb-10'>Gabung dan nikmati kemudahan layanan kami</p>
-
-//             <FormInput 
-//               labelText="Nama lengkap"
-//               inputType="text"
-//               name="nama"
-//               placeholder="John Doe"
-//               isFirstItem={true} />
-
-//             <div className='w-full flex gap-x-6 flex-row-reverse'>
-//               <FormSelect 
-//                 labelText="Jenis kelamin"
-//                 name='jenisKelamin'
-//                 placeholder="masukan jenis kelamin"
-//                 list={['Pria', 'Wanita']}
-//                 isFirstItem={false}
-//               />
-
-//               <FormInput  
-//                 labelText="Email"
-//                 inputType="email"
-//                 name="email"
-//                 placeholder="enter your email"
-//                 isFirstItem={false} />
-//             </div>
-
-//             <FormInput  
-//               labelText="Password"
-//               inputType={showPass ? 'text' : 'password'}
-//               name='password'
-//               placeholder="enter your password"
-//               isFirstItem={false} />
-          
-//             <div className='w-full flex justify-between'>
-//               <section className='flex justify-center flex-row gap-x-4'>
-//                 <input name='showpass' className='w-4 h-4' type="checkbox" onChange={() => setShowPass(!showPass)}/>
-//                 <label htmlFor="showpass" className='text-[12px]'>show password</label>
-//               </section>
-
-//               <p className='text-right text-[12px] cursor-default underline text-grey lowercase mb-10'>lupa password ?</p>
-
-//             </div>
-
-//           <button type='submit' className='w-full text-md text-grey flex items-center justify-center font-semibold tracking-wider cursor-default hover:bg-blue disabled:bg-blue duration-200 ease-in-out bg-blue/80 py-3 rounded-md'>
-//             { isSubmitting ? <LoaderCircle className='animate-spin stroke-white' /> : 'Register' }
-//           </button>
-//           <p className='text-grey text-center text-[12px] mt-4'>Sudah punya akun? <Link to='/login' className='underline'>Daftar disini</Link></p>
-
-//         </div>
-//     </Form>
-
-
-//   <section className="col-span-1 bg-green-100">
-//     <img src={registerPhoto} alt="" />
-//   </section>
-// </div>
