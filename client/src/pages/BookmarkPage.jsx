80313192ce18c7da9d087c4e5341e05c70381dff
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-14 23:12:46 +0800

    bookmark page integration

--- client/src/pages/BookmarkPage.jsx
+++ client/src/pages/BookmarkPage.jsx
@@ -1,7 +1,7 @@
-import ArticelCards from "../components/ArticelCards"
+import { ArticleCards } from '../components'
 import customFetch from '../utils/customFetch'
-import { Trash } from 'lucide-react'
-import { redirect, useLoaderData, Form } from "react-router-dom"
+import { LoaderCircle } from 'lucide-react'
+import { redirect, useLoaderData, Form, useNavigation } from "react-router-dom"
 import { toast } from 'react-toastify'
 
 export const loader = async() => {
@@ -29,12 +29,27 @@ export const action = async({ request }) => {
 const BookmarkPage = () => {
 
   const { bookmarked } = useLoaderData()
+  const isLoading = useNavigation().state === 'loading'
   
   return (
     <section className='w-full h-full overflow-y-auto p-10 flex items-center justify-center bg-slate-50'>
       <section className='w-full h-full '>
         <h1 className='text-3xl text-slate-800 font-semibold'>Artikel Tersimpan Anda</h1>
         <p className='text-slate-600 w-[80%] mt-2'>Semua artikel yang Anda simpan ada di sini. Temukan kembali informasi berharga dengan mudah kapan saja.</p>
+
+        <article className={`w-full mt-10 flex flex-wrap items-stretch ${bookmarked.length < 4 ? 'justify-start' : 'justify-between'} gap-6`}>
+        { isLoading ? (
+            <div className='w-full flex-1 flex items-center justify-center gap-x-4'>
+              <LoaderCircle className='w-6 h-6 stroke-slate-800 animate-spin' />
+              <h1 className='text-center text-xl font-medium text-slate-700'>Sedang memuat artikel</h1>
+            </div>
+
+          ) : (
+            bookmarked.map((item, index) => {
+                return <ArticleCards {...item} key={index}/>
+              })
+          )}
+        </article>
       </section>  
     </section>
   )
