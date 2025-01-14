6482eadc934c612b6b4173e62fe95e27d9bac9f1
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-14 10:36:35 +0800

    setup login page refactor

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -43,50 +43,57 @@ const Login = () => {
   const isSubmitting = useNavigation().state === 'submitting'
 
   return (
-    <div className='w-fullScreen h-fullScreen grid grid-cols-12'>
+    <section className='w-fullScreen h-fullScreen p-20 flex items-center justify-center'>
 
-        <Form method="POST" className="w-full h-full flex justify-center items-center col-span-6 bg-[url('/src/assets/images/gradient.jpg')] bg-no-repeat bg-cover">
-            <div className="px-6 py-10 w-[70%]">
-              <h1 className='font-semibold text-2xl'>Selamat datang kembali</h1>
-              <p className='text-sm mt-2 mb-10'>Akses akun Anda untuk melanjutkan</p>
+      <section className='w-full h-full mt-4 bg-slate-200 rounded-xl shadow-md'>
+        <h1>This is main container</h1>
+      </section>
 
-                <FormInput  
-                  labelText="Email"
-                  inputType="email"
-                  name="email"
-                  placeholder="enter your email"
-                  isFirstItem={true} />
-
-                <FormInput  
-                  labelText="Password"
-                  inputType={showPass ? 'text' : 'password'}
-                  name='password'
-                  placeholder="enter your password"
-                  isFirstItem={false} />
-              
-                <div className='w-full flex justify-between'>
-                  <section className='flex justify-center flex-row gap-x-4'>
-                    <input name='showpass' className='w-4 h-4' type="checkbox" onChange={() => setShowPass(!showPass)}/>
-                    <label htmlFor="showpass" className='text-[12px]'>show password</label>
-                  </section>
-
-                  <p className='text-right text-[12px] cursor-default text-grey lowercase mb-10 underline'>lupa password?</p>
-
-                </div>
-
-              <button disabled={isSubmitting} className='w-full flex items-center justify-center gap-x-6 text-md text-grey font-semibold tracking-wider cursor-default hover:bg-blue duration-200 ease-in-out bg-blue/80 disabled:bg-blue py-3 rounded-md'>
-                { isSubmitting ?  <LoaderCircle className='stroke-white animate-spin ' /> : 'Login'}
-              </button>
-              <p className='text-grey text-center text-[12px] mt-4'>Belum punya akun? <Link to='/register' className='underline'>Daftar disini</Link></p>
-
-            </div>
-        </Form>
-
-        <section className='w-full h-full bg-green-100 col-span-6 overflow-hidden'>
-            <img className='h-fit' src={aboutPhoto} alt="" />
-        </section>
-    </div>
+    </section>
   )
 }
 
 export default Login
+    // <div className='w-fullScreen h-fullScreen grid grid-cols-12'>
+
+    //     <Form method="POST" className="w-full h-full flex justify-center items-center col-span-6 bg-[url('/src/assets/images/gradient.jpg')] bg-no-repeat bg-cover">
+    //         <div className="px-6 py-10 w-[70%]">
+    //           <h1 className='font-semibold text-2xl'>Selamat datang kembali</h1>
+    //           <p className='text-sm mt-2 mb-10'>Akses akun Anda untuk melanjutkan</p>
+
+    //             <FormInput  
+    //               labelText="Email"
+    //               inputType="email"
+    //               name="email"
+    //               placeholder="enter your email"
+    //               isFirstItem={true} />
+
+    //             <FormInput  
+    //               labelText="Password"
+    //               inputType={showPass ? 'text' : 'password'}
+    //               name='password'
+    //               placeholder="enter your password"
+    //               isFirstItem={false} />
+              
+    //             <div className='w-full flex justify-between'>
+    //               <section className='flex justify-center flex-row gap-x-4'>
+    //                 <input name='showpass' className='w-4 h-4' type="checkbox" onChange={() => setShowPass(!showPass)}/>
+    //                 <label htmlFor="showpass" className='text-[12px]'>show password</label>
+    //               </section>
+
+    //               <p className='text-right text-[12px] cursor-default text-grey lowercase mb-10 underline'>lupa password?</p>
+
+    //             </div>
+
+    //           <button disabled={isSubmitting} className='w-full flex items-center justify-center gap-x-6 text-md text-grey font-semibold tracking-wider cursor-default hover:bg-blue duration-200 ease-in-out bg-blue/80 disabled:bg-blue py-3 rounded-md'>
+    //             { isSubmitting ?  <LoaderCircle className='stroke-white animate-spin ' /> : 'Login'}
+    //           </button>
+    //           <p className='text-grey text-center text-[12px] mt-4'>Belum punya akun? <Link to='/register' className='underline'>Daftar disini</Link></p>
+
+    //         </div>
+    //     </Form>
+
+    //     <section className='w-full h-full bg-green-100 col-span-6 overflow-hidden'>
+    //         <img className='h-fit' src={aboutPhoto} alt="" />
+    //     </section>
+    // </div>
