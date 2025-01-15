b20ab7a52a6fff48fd9b5e14dd0380fcd5c6870c
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-16 00:13:53 +0800

    redesign all articles page

---
+++ client/src/components/ArticlesCardsAdmin.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import moment from 'moment'
+import { Clock, ThumbsUp, Pin, Pencil, Trash } from 'lucide-react'
+import { Form, Link } from 'react-router-dom'
+
+const ArticlesCardsAdmin = ({ _id, judulArtikel, createdAt, deskripsi, jenisArtikel, thumbnail }) => {
+  judulArtikel = judulArtikel.length >= 70 ? judulArtikel.slice(0, 67) + '...' : judulArtikel
+  deskripsi = deskripsi.length >= 70 ? deskripsi.slice(0, 115) + '...' : deskripsi
+  const formattedTime = moment(createdAt).startOf('hour').fromNow(); 
+
+  return (
+    <section className='max-w-[23%] max-h-[50vh] bg-white shadow-md p-2 rounded-xl flex flex-col justify-between cursor-default'>
+        <Link to={`/admin/dashboard/news/${judulArtikel}`} className='w-full'>
+          <img className='w-full overflow-hidden object-fill object-top rounded-xl min-h-32 bg-slate-500' src={thumbnail} alt="" />
+
+          <p className='text-xs text-slate-700 flex items-center justify-start gap-x-4 my-4'>
+            <Clock className='w-4 h-4 stroke-slate-400' />
+            {formattedTime}
+          </p>
+
+          <h1 className='text-slate-800 text-sm font-semibold'>{judulArtikel}</h1>
+          <p className='text-xs text-slate-600 mt-2 flex-grow'>{deskripsi}</p>
+        </Link>
+
+        <div className='w-full mt-4 flex items-center justify-between'>
+          <p className='lowercase py-1 px-4 rounded-md text-xs bg-blue/40 '>{jenisArtikel}</p>
+
+          <article className='flex gap-x-2 justify-end items-center mt-auto'>
+            <Form method='POST' className='flex items-center'>
+              <input type="hidden" name='id' value={_id} />
+              <button type='submit' name='data' value='favorite' className='m-0 p-0 border-none bg-transparent'>
+                <Pencil className={`border-[2px] bg-blue/80 border-transparent stroke-white duration-200 ease-in-out p-1 rounded-md w-6 h-6`} />
+              </button>
+            </Form>
+            
+            <Form method='POST' className='flex items-center'>
+              <input type="hidden" name='id' value={_id} />
+              <button type='submit' name='data' value='bookmark' className='m-0 p-0 border-none bg-transparent'>
+                <Trash className={`border-[2px] border-transparent bg-pink-400 stroke-white  duration-200 ease-in-out p-1 rounded-md w-6 h-6`} />
+              </button>
+            </Form>
+          </article>
+        </div>
+    </section>
+  )
+}
+
+export default ArticlesCardsAdmin
--- client/src/pages/AllNews.jsx
+++ client/src/pages/AllNews.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Search, X, FilePenLine, Trash2 } from 'lucide-react'
-import { ArticleCards, Loading } from '../components'
+import { ArticleCardsAdmin, Loading } from '../components'
 import customFetch from '../utils/customFetch'
 import { toast } from 'react-toastify'
 import { Form, useLoaderData, useNavigation, Link, useNavigate } from 'react-router-dom'
@@ -77,7 +77,43 @@ const AllNews = () => {
         <h1 className='text-3xl text-slate-800 font-semibold'>Kelola Semua Artikel</h1>
         <p className='text-slate-600 w-[80%] mt-2'>Di sini Anda dapat melihat, mengedit, dan mengelola semua artikel yang telah dibuat. Pantau status artikel untuk memastikan konten selalu relevan dan up-to-date</p>
 
-        
+        <article className='w-full mt-10 flex items-end gap-x-4 justify-normal'>
+          <div className='w-[75%] mt-4'>
+            <div className='w-full flex items-center justify-between h-10 rounded-xl border-[2px] border-slate-400 focus-within:border-blue/80'>
+              <Search className='stroke-slate-600 w-5 h-5 ml-2' />
+              <input type="text" name="email" id="email" className='w-full px-6 h-full text-md outline-none focus:placeholder:text-transparent placeholder:text-sm text-slate-800' autoComplete='off' placeholder='tips mengurangi berat badan '  required autoCorrect='off' onChange={(e) => setIsSearch(e.target.value)} value={isSearch}/>
+              <X onClick={() => setIsSearch('')} className={` ${isSearch ? 'visible' : 'invisible'} stroke-red-400 w-5 h-5 mr-2`} />
+            </div>
+          </div>
+
+          <select name="filter" id="filter" className='flex-1 h-10 border-[2px] border-slate-400 rounded-xl text-sm px-4 outline-none focus:border-blue/80' onChange={(e) => setFilter(e.target.value)}>
+            <option value="">Semua</option>
+            <option value="Diabetes">Diabetes</option>
+            <option value="Obesitas">Obesitas</option>
+          </select>
+        </article>
+
+        <article className={`w-full mt-10 flex flex-wrap items-stretch ${data.news?.length < 4 ? 'justify-start' : 'justify-between'} gap-6`}>
+
+          { isLoading ? (
+            <div className='w-full flex-1 flex items-center justify-center gap-x-4'>
+              <LoaderCircle className='w-6 h-6 stroke-slate-800 animate-spin' />
+              <h1 className='text-center text-xl font-medium text-slate-700'>Sedang memuat artikel</h1>
+            </div>
+
+          ) : (
+            data.news.filter((newItem) => {
+              if (filter) {
+                return newItem.jenisArtikel === filter
+              } else {
+                return newItem
+              }
+              }).map((item, index) => {
+                return <ArticleCardsAdmin key={index} {...item} />
+              })
+          )}
+
+        </article>
       </section>  
     </section>
   )
