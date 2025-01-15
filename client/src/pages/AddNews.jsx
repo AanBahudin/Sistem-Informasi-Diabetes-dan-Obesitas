4bd0e86c60f6149c073c3b990c3bd6e749aea3cf
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-15 23:59:20 +0800

    add news page setup

--- client/src/pages/AddNews.jsx
+++ client/src/pages/AddNews.jsx
@@ -47,119 +47,127 @@ const AddNews = () => {
   
 
   return (
-    <Form method="POST" encType='multipart/form-data' className='w-full h-fit p-10 overflow-y-auto'>
+    <section className='w-full h-full overflow-y-auto p-10 flex items-center justify-center bg-slate-50'>
+      <section className='w-full h-full '>
+        <h1 className='text-3xl text-slate-800 font-semibold'>Buat Artikel Baru</h1>
+        <p className='text-slate-600 w-[80%] mt-2'>Mulai tulis artikel baru di sini. Sampaikan informasi berharga untuk membantu pengguna memahami lebih banyak tentang kesehatan, obesitas, dan diabetes.</p>
 
-      <h1 className='text-2xl font-medium '>Buat Artikel Baru</h1>
-      <h5 className='mt-12 text-xl font-medium border-b-[2px] pb-2 border-gray-500/50'>Informasi umum</h5>
-      <section className='w-full grid grid-cols-3 my-6 gap-x-10'>
-        <FormInput 
-          inputType='text'
-          isFirstItem={true}
-          labelText="Judul artikel"
-          name='judulArtikel'
-          placeholder='Cara mengurangi berat badan...'
-          useBorder={true}
-          />
-
-        <FormInput 
-          inputType='text'
-          isFirstItem={true}
-          labelText="Tag Artikel"
-          name='tagArtikel'
-          placeholder='Pola makan'
-          useBorder={true}
-          /> 
-
-        <FormInput 
-          inputType='text'
-          labelText="Penyuting Artikel"
-          name='penyutingArtikel'
-          placeholder='Dr. Kino Kinora'
-          useBorder={true}
-        />
-
-        <FormSelect 
-          defaultValue=''
-          list={['Diabetes', 'Obesitas']}
-          labelText='Jenis Artikel'
-          name='jenisArtikel'
-          useBorder = {true}
-          />
-
-        <FormInput 
-          inputType='file'
-          labelText="Gambar Thumbnail"
-          name='thumbnail'
-          id='thumbnail'
-          placeholder='Cara mengurangi berat badan...'
-          useBorder={true}
-          />
-      </section>
- 
-      <section className='w-full grid grid-cols-3 gap-x-4 place-items-stretch'>
-        <TextAreaInput 
-          name='deskripsi'
-          defaultValue=''
-          labelText='deskripsi'
-          placeholder='isi deskripsi artikel ...' 
-        />
         
-        <TextAreaInput 
-          name='tagar'
-          defaultValue=''
-          labelText='Tagar'
-          placeholder='#polahidup #kesehatan #diabetes' 
-        />
-
-        <TextAreaInput 
-          name='referensi'
-          defaultValue=''
-          labelText='Referensi'
-          placeholder='Newman, D. Nourish by WebMD (2018). The Benefits of Eating Breakfast.' 
-        />
-      </section>
-
-      {/* editor anjay */}
-      <h5 className='mt-6 text-xl font-medium border-b-[2px] pb-2 border-gray-500/50'>Struktur Artikel</h5>
-      <section className='w-full mt-6 .rdw-editor-wrapper'>
-        <div className="w-[95%] mx-auto bg-white h-[70vh] overflow-y-auto rounded-lg border-[1px] border-grey px-6 wysiwyg-container rdw-editor-wrapper">
-          <Editor
-            placeholder="Start typing articles..."
-            toolbar={{
-              options: [
-                'inline',
-                'blockType',
-                'fontSize',
-                'list',
-                'textAlign',
-                'link',
-                'embedded',
-                'image',
-                'remove',
-                'history',
-              ], // Hilangkan fontFamily dari daftar
-              indent: {
-                options: ['indent', 'outdent']
-              },
-            }}
-            editorState={editorState}
-            wrapperClassName="wrapperClassName"
-            editorClassName="editorClassName"
-            onEditorStateChange={onEditorStateChange} // Gunakan fungsi ini untuk menangani perubahan
-          />
-
-          <input type="hidden" name='editorContent' value={editorContent} />
-        </div>
-      </section>
-
-      <section className='flex items-center justify-end gap-x-6'>
-        <button type='reset' className='bg-red-500 text-sm px-10 py-3 rounded-md mt-6 w-48'>Reset</button>
-        <button type='submit' disabled={isSubmitting} className='bg-blue text-sm px-10 py-3 rounded-md mt-6 w-48'>{ isSubmitting ? 'Membuat ...' : 'Publish' }</button>
-      </section>
-    </Form>
+      </section>  
+    </section>
   );
 };
 
 export default AddNews;
+    // <Form method="POST" encType='multipart/form-data' className='w-full h-fit p-10 overflow-y-auto'>
+
+    //   <h1 className='text-2xl font-medium '>Buat Artikel Baru</h1>
+    //   <h5 className='mt-12 text-xl font-medium border-b-[2px] pb-2 border-gray-500/50'>Informasi umum</h5>
+    //   <section className='w-full grid grid-cols-3 my-6 gap-x-10'>
+    //     <FormInput 
+    //       inputType='text'
+    //       isFirstItem={true}
+    //       labelText="Judul artikel"
+    //       name='judulArtikel'
+    //       placeholder='Cara mengurangi berat badan...'
+    //       useBorder={true}
+    //       />
+
+    //     <FormInput 
+    //       inputType='text'
+    //       isFirstItem={true}
+    //       labelText="Tag Artikel"
+    //       name='tagArtikel'
+    //       placeholder='Pola makan'
+    //       useBorder={true}
+    //       /> 
+
+    //     <FormInput 
+    //       inputType='text'
+    //       labelText="Penyuting Artikel"
+    //       name='penyutingArtikel'
+    //       placeholder='Dr. Kino Kinora'
+    //       useBorder={true}
+    //     />
+
+    //     <FormSelect 
+    //       defaultValue=''
+    //       list={['Diabetes', 'Obesitas']}
+    //       labelText='Jenis Artikel'
+    //       name='jenisArtikel'
+    //       useBorder = {true}
+    //       />
+
+    //     <FormInput 
+    //       inputType='file'
+    //       labelText="Gambar Thumbnail"
+    //       name='thumbnail'
+    //       id='thumbnail'
+    //       placeholder='Cara mengurangi berat badan...'
+    //       useBorder={true}
+    //       />
+    //   </section>
+ 
+    //   <section className='w-full grid grid-cols-3 gap-x-4 place-items-stretch'>
+    //     <TextAreaInput 
+    //       name='deskripsi'
+    //       defaultValue=''
+    //       labelText='deskripsi'
+    //       placeholder='isi deskripsi artikel ...' 
+    //     />
+        
+    //     <TextAreaInput 
+    //       name='tagar'
+    //       defaultValue=''
+    //       labelText='Tagar'
+    //       placeholder='#polahidup #kesehatan #diabetes' 
+    //     />
+
+    //     <TextAreaInput 
+    //       name='referensi'
+    //       defaultValue=''
+    //       labelText='Referensi'
+    //       placeholder='Newman, D. Nourish by WebMD (2018). The Benefits of Eating Breakfast.' 
+    //     />
+    //   </section>
+
+    //   {/* editor anjay */}
+    //   <h5 className='mt-6 text-xl font-medium border-b-[2px] pb-2 border-gray-500/50'>Struktur Artikel</h5>
+    //   <section className='w-full mt-6 .rdw-editor-wrapper'>
+    //     <div className="w-[95%] mx-auto bg-white h-[70vh] overflow-y-auto rounded-lg border-[1px] border-grey px-6 wysiwyg-container rdw-editor-wrapper">
+    //       <Editor
+    //         placeholder="Start typing articles..."
+    //         toolbar={{
+    //           options: [
+    //             'inline',
+    //             'blockType',
+    //             'fontSize',
+    //             'list',
+    //             'textAlign',
+    //             'link',
+    //             'embedded',
+    //             'image',
+    //             'remove',
+    //             'history',
+    //           ], // Hilangkan fontFamily dari daftar
+    //           indent: {
+    //             options: ['indent', 'outdent']
+    //           },
+    //         }}
+    //         editorState={editorState}
+    //         wrapperClassName="wrapperClassName"
+    //         editorClassName="editorClassName"
+    //         onEditorStateChange={onEditorStateChange} // Gunakan fungsi ini untuk menangani perubahan
+    //       />
+
+    //       <input type="hidden" name='editorContent' value={editorContent} />
+    //     </div>
+    //   </section>
+
+    //   <section className='flex items-center justify-end gap-x-6'>
+    //     <button type='reset' className='bg-red-500 text-sm px-10 py-3 rounded-md mt-6 w-48'>Reset</button>
+    //     <button type='submit' disabled={isSubmitting} className='bg-blue text-sm px-10 py-3 rounded-md mt-6 w-48'>{ isSubmitting ? 'Membuat ...' : 'Publish' }</button>
+    //   </section>
+    // </Form>
 
 
