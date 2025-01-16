e1d0a2e91088d3bb0006fda77f51f788243a4205
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-16 11:16:59 +0800

    navbar refactor

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,15 +22,15 @@ import { Link, NavLink } from 'react-router-dom';
     }, []);
 
    return (
-    <div className='py-8 mx-auto px-16 flex justify-between z-10 fixed w-[90%] transition-colors duration-75 bg-lightGrey rounded-b-[8px]'
-    style={{ backgroundColor: `rgba(244, 246, 255, ${bgOpacity})` }}>
-        <Link to='/' className='text-2xl font-semibold  cursor-default'>E-Health</Link>
+    <div className='py-8 mx-auto px-16 flex justify-between z-10 fixed w-[90%] transition-colors duration-75 rounded-b-[8px]'
+    style={{ backgroundColor: `rgba(241, 245, 249, ${bgOpacity})` }}>
+        <Link to='/' className='text-2xl font-semibold text-slate-900 cursor-default'>E-Health</Link>
 
         <section className='flex flex-row gap-x-7 items-center'>
-            <a href='/#contacts' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Kontak</a>
-            <a href='/#services' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Layanan</a>
-            <a href='/artikel' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Artikel</a>
-            <NavLink to='/login' className={({isActive}) => `${isActive ? 'hidden' : 'visible'} bg-blue px-8 py-2 text-sm rounded-md no-underline text-grey`} end>
+            <a href='/#contacts' className='text-slate-800 text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Kontak</a>
+            <a href='/#services' className='text-slate-800 text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Layanan</a>
+            <a href='/artikel' className='text-slate-800 text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Artikel</a>
+            <NavLink to='/login' className={({isActive}) => `${isActive ? 'hidden' : 'visible'} bg-blue/80 text-slate-800 px-8 py-2 text-sm rounded-md no-underline`} end>
                 {data ? 'Dashboard' : 'Login'}
             </NavLink>
         </section>
