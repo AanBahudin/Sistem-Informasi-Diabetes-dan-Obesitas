82edd40f9f16e0f7d3197171dda9dc13ba6154ff
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-16 10:01:22 +0800

    navbar link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
  
  const Navbar = ({data}) => {
     
@@ -24,12 +24,12 @@ import { NavLink } from 'react-router-dom';
    return (
     <div className='py-8 mx-auto px-16 flex justify-between z-10 fixed w-[90%] transition-colors duration-75 bg-lightGrey rounded-b-[8px]'
     style={{ backgroundColor: `rgba(244, 246, 255, ${bgOpacity})` }}>
-        <h1 className='text-2xl font-semibold  cursor-default'>E-Health</h1>
+        <Link to='/' className='text-2xl font-semibold  cursor-default'>E-Health</Link>
 
         <section className='flex flex-row gap-x-7 items-center'>
-            <a href='/#contacts' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Contacts</a>
-            <a href='/#services' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Services</a>
-            <a href='/#article' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Articles</a>
+            <a href='/#contacts' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Kontak</a>
+            <a href='/#services' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Layanan</a>
+            <a href='/artikel' className='text-grey text-sm cursor-default hover:text-blue duration-200 ease-in-out no-underline'>Artikel</a>
             <NavLink to='/login' className={({isActive}) => `${isActive ? 'hidden' : 'visible'} bg-blue px-8 py-2 text-sm rounded-md no-underline text-grey`} end>
                 {data ? 'Dashboard' : 'Login'}
             </NavLink>
