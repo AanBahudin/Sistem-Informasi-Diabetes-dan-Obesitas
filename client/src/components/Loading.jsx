d19b636fbc3ce3ff4b42cbb2253f1b6a84dc756f
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-31 15:37:41 +0800

    loading position fix

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='w-full h-[100vh] flex flex-col items-center justify-center'>
-        <h1 className='text-center text-4xl font-semibold animate-pulse'>Loading ....</h1>
+        <h1 className='text-center text-slate-800 text-4xl font-semibold animate-pulse'>Loading ....</h1>
     </div>
   )
 }
